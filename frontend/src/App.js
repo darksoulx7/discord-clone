496397f17db28091f8fee691bd090d2cf8c99039
Vishvas Prajapati <vishvas.prajapati@volansys.com>
2023-05-30 21:18:28 +0530

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Redirect,
+// } from "react-router-dom";
+// import LoginPage from "./authPages/LoginPage/LoginPage";
+// import RegisterPage from "./authPages/RegisterPage/RegisterPage";
+// import Dashboard from "./Dashboard/Dashboard";
+// import AlertNotification from "./shared/components/AlertNotification";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
