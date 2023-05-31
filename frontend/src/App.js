6e59b76de1840f544813f85d6f888260b6b6475c
Vishvas Prajapati <vishvas.prajapati@volansys.com>
2023-05-31 21:06:52 +0530

    dashboard setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
 import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Redirect,
-// } from "react-router-dom";
-// import LoginPage from "./authPages/LoginPage/LoginPage";
-// import RegisterPage from "./authPages/RegisterPage/RegisterPage";
-// import Dashboard from "./Dashboard/Dashboard";
-// import AlertNotification from "./shared/components/AlertNotification";
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes
+} from "react-router-dom";
+import LoginPage from "./authPages/LoginPage/LoginPage";
+import RegisterPage from "./authPages/RegisterPage/RegisterPage";
+import Dashboard from "./Dashboard/Dashboard";
+import AlertNotification from "./shared/components/AlertNotification";
 
 import "./App.css";
 
 function App() {
-  return (
-    <>
-
+  return <>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<Dashboard />}/ >
+          <Route path="*" element={<Navigate to="/dashboard" />}
+          />
+        </Routes>
+      </Router>
+      <AlertNotification />
     </>
-  );
 }
 
 export default App;
---
+++ frontend/src/api.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { logout } from "./shared/utils/auth";
+
+const apiClient = axios.create({
+  baseURL: "http://localhost:9090/api",
+  timeout: 4000,
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const userDetails = localStorage.getItem("user");
+
+    if (userDetails) {
+      const token = JSON.parse(userDetails).token;
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+// public routes
+export const login = async (data) => {
+  try {
+    return await apiClient.post("/auth/login", data);
+  } catch (exception) {
+    return {
+      error: true,
+      exception,
+    };
+  }
+};
+
+export const register = async (data) => {
+  try {
+    return await apiClient.post("/auth/register", data);
+  } catch (exception) {
+    return {
+      error: true,
+      exception,
+    };
+  }
+};
+
+// secure routes
+
+// eslint-disable-next-line no-unused-vars
+const checkResponseCode = (exception) => {
+  const responseCode = exception?.response?.status;
+
+  if (responseCode) {
+    (responseCode === 401 || responseCode === 403) && logout();
+  }
+};
